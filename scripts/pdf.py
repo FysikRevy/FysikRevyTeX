286918f5738369f587916fab33d0998b7d65c497
theunbound <the.bentusi@gmail.com>
2022-04-08 01:32:48 +0200

    pdf-merging omskrevet. fjernet afhængighed af blank.pdf
    også indført mulighed for at afgøre, om hver enkelt pdf må starte på
    en verso-side.

--- scripts/pdf.py
+++ scripts/pdf.py
@@ -3,7 +3,7 @@ import os
 import subprocess
 from multiprocessing import Pool, cpu_count
 
-from PyPDF2 import PdfFileMerger,PdfFileReader
+from PyPDF2 import PdfFileWriter,PdfFileReader
 
 from config import configuration as conf
 
@@ -16,9 +16,15 @@ class PDF:
         return self._pdfmerge(*args, **kwargs)
 
     def _pdfmerge(self, file_list, pdfname):
-        """Merge a list of PDF files.  
-file_list kan være en liste med filnavne, eller en liste med tupler af
-filnavne og bogmærkenavne (til indholdsfortegnelsen)."""
+        """Merge a list of PDF files.  file_list kan være en liste med
+filnavne, eller en liste med tupler af formatet:
+( filnavn, bookmark = None, verso = False )
+
+Hvor filnavn er filnavnet, bookmark er navnet på et pdf-bogmærke (til
+indholdsfortegnelsen) og verso angiver, om pdf-filen må starte
+på en verso-side i dobbeltsidet layout.
+
+        """
         # det er egentlig noget hø, sådan automagisk at bøje sig rundt
         # om forskellige typer i argumenterne. Det nærmest tigger og
         # be'r om at blive til en bug i fremtiden. Men det giver
@@ -32,95 +38,82 @@ filnavne og bogmærkenavne (til indholdsfortegnelsen)."""
             # ingen output er ikke nyere end nogen ting
             output_modtime = float('-inf')
 
-        merge_args = ()         # argumenter til PyPDF2 append()
-        found_fresh_input = False # flag (::sadface::)
+        def gen_arg_list( args ):
+            if not isinstance( args, tuple ):
+                args = args,
+            f, bookmark, verso = args + ( "", None, False )[len(args):]
+            
+            if type( f ) == str and f[-3:] == "pdf":
+                return ( f, bookmark, verso ),
 
-        for fe in file_list:
-            f, bookmark = fe if isinstance( fe, tuple ) else ( fe, None )
-            if type(f) == str and f[-3:] == 'pdf':
-                merge_args += ({ "fileobj": f,
-                                "bookmark": bookmark
-                                },)
-                found_fresh_input |= (
-                    os.stat( f ).st_mtime > output_modtime
-                )
-                if PdfFileReader( f ).numPages % 2 == 1:
-                    merge_args += { "fileobj": "blank.pdf", "bookmark": None },
-
-
-
-            elif type(f).__name__ == "Revue":
-                for act in f.acts:
-                    for m in act.materials:
-                        pdfpath = os.path.join(
+            if type( f ).__name__ == "Revue":
+                return (
+                    (
+                        os.path.join(
                             self.conf["Paths"]["pdf"],
                             os.path.dirname( os.path.relpath( m.path )),
                             os.path.splitext( m.file_name )[0] + ".pdf"
-                        )
-                        merge_args += ({
-                            "fileobj": pdfpath,
-                            "bookmark": bookmark or m.title or None
-                        },)
-                        found_fresh_input |= (
-                            os.path.getmtime( pdfpath ) > output_modtime
-                        )
-                        if PdfFileReader( pdfpath ).numPages % 2 == 1:
-                            merge_args += { "fileobj": "blank.pdf", "bookmark": None },
+                        ),
+                        ( bookmark or m.title or None ),
+                        verso )
+                    for act in f.acts
+                    for m in act.materials
+                )
 
+            if type( f ).__name__ == "Actor":
+                return (
+                    (
+                        os.path.join(
+                            self.conf["Paths"]["pdf"],
+                            os.path.dirname( os.path.relpath( role.material.path ) ),
+                            os.path.splitext( role.material.file_name )[0] + ".pdf"
+                        ),
+                        ( bookmark or role.material.title or None ),
+                        verso )
+                    for role in f.roles
+                )
 
-            
-            elif type(f).__name__ == "Actor":
-                for role in f.roles:
-                    pdfpath = os.path.join(
-                        self.conf["Paths"]["pdf"],
-                        os.path.dirname( os.path.relpath( role.material.path )),
-                        os.path.splitext( role.material.file_name )[0] + ".pdf"
-                    )                    
-                    merge_args += ({
-                        "fileobj": pdfpath,
-                        "bookmark": bookmark or role.material.title or None
-                    },)
-                    found_fresh_input |= (
-                        os.path.getmtime( pdfpath ) > output_modtime
-                    )
-                    if PdfFileReader( pdfpath ).numPages % 2 == 1:
-                        merge_args += { "fileobj": "blank.pdf", "bookmark": None },
+            # otherwise...
+            print( f, bookmark, verso )
+            raise TypeError("List must only contain PDF file paths, "
+                            "a Revue object or an Actor object.")
 
+        arg_list = [ arg for f in file_list for arg in gen_arg_list( f ) ]
 
-
-            else:
-                raise TypeError("List must only contain PDF file paths, "
-                                "a Revue object or an Actor object.")
-
-        if ( not found_fresh_input
-             and not self.conf.getboolean(
-                 "TeXing", "force TeXing of all files"
-             )
-            ):
+        if not self.conf.getboolean( "TeXing", "force TeXing of all files" )\
+           and not any( os.path.getmtime( f ) > output_modtime
+                        for f,_,_ in arg_list ):
             return              # intet nyt ind = intet nyt ud
 
-        merger = PdfFileMerger()
-
         # så kører bussen
+        writer = PdfFileWriter()
+        writer.setPageLayout( "/TwoPageRight" )
+        writer.setPageMode( "/UseOutlines" )
+
         tex_translations = { "\\texttrademark": "™",
                              "--": "–",
                              "---": "—",
                              "''": "“",
                              "``": "”"
                             }
-        for kwargs in merge_args:
-            for t in tex_translations:
-                if kwargs[ "bookmark" ]:
-                    kwargs[ "bookmark" ] = ( kwargs[ "bookmark" ]
-                                             .replace( t, tex_translations[t] )
-                                            )
-            merger.append( **kwargs )
-            merger.setPageLayout( "/TwoPageRight" )
-            merger.setPageMode( "/UseOutlines" )
+        for filename, bookmark, verso in arg_list:
+            pagenum = writer.getNumPages()
+            if pagenum % 2 == 1 and not verso:
+                writer.addBlankPage()
+                
+            inpdf = PdfFileReader( filename )
+            writer.appendPagesFromReader( inpdf )
+            
+            if bookmark:
+                for t in tex_translations:
+                    bookmark = bookmark.replace( t, tex_translations[t] )
+
+                writer.addBookmark( bookmark, pagenum )
+            
 
         try:
             with open(pdfname, "wb") as output:
-                merger.write(output)
+                writer.write(output)
                 output.close()
         except FileNotFoundError:
             err = "File could not be opened: {}".format(pdfname)
@@ -130,7 +123,9 @@ filnavne og bogmærkenavne (til indholdsfortegnelsen)."""
         else:
             rc = 0
 
-        if not self.conf["TeXing"]["pdfsizeopt"] == "no":
+        try:
+            self.conf.getboolean( "TeXing", "pdfsizeopt" )
+        except ValueError:
             subprocess.run([ self.conf["TeXing"]["pdfsizeopt"],
                              os.path.abspath( pdfname ),
                              os.path.abspath( pdfname ) ],
@@ -140,7 +135,12 @@ filnavne og bogmærkenavne (til indholdsfortegnelsen)."""
                            capture_output = True
                            )
             
-        print("{:<42}".format("\033[0;37;1m{}:\033[0m".format(os.path.split(pdfname)[1])), end="")
+        print(
+            "{:<42}".format(
+                "\033[0;37;1m{}:\033[0m".format(os.path.split(pdfname)[1])
+            ),
+            end=""
+        )
         if rc == 0:
             print("\033[0;32m Success!\033[0m")
         else:
