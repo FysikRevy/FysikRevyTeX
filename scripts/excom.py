9674e191ce34c80f3545437d02421386af1eec8b
theunbound <the.bentusi@gmail.com>
2024-09-12 23:38:28 +0200

    nej, det virkede kun for MikTeX

--- scripts/excom.py
+++ scripts/excom.py
@@ -1,6 +1,6 @@
 #  coding; utf-8
 
-import subprocess
+import os, subprocess
 from pathlib import Path
 
 from config import configuration as conf
@@ -20,19 +20,25 @@ class PopenGen( subprocess.Popen ):
          if self.returncode != None:
             return
 
-def tex( texfile, outputfile=None, auxdir=None, searchdir=None ):
-   output_args = [ "-output-directory=" + str( Path( outputfile ).parent ),
-                   "-job-name=" + str( Path( outputfile ).stem )
-                  ] if outputfile else []
-   dir_args = [ k + str( v ) for k,v in { "-aux-directory=": auxdir,
-                                          "-include-directory=": searchdir
-                                         }.items()
-                if v
-               ]
+def tex( texfile, outputname=None, searchdir=None ):
+   texinputs = ":".join( path.as_posix + "/" for path
+                         in ( Path( texfile ).parent,
+                              Path( searchdir )
+                             )
+                         if path != None
+                        ) + ":"
+   try:
+      texinputs += os.environ["TEXINPUTS"]
+   except KeyError:
+      pass
+   env = { **os.environ, **{"TEXINPUTS": texinputs } }
+   jobname = [ "-job-name=" + str( Path( outputname ).stem ) ]\
+      if outputname else []
 
    return PopenGen(
       [ conf["TeXing"]["tex command"], "-interaction=nonstopmode" ]\
-        + output_args + dir_args + [ texfile ],
+        + jobname + [ str( texfile ) ],
+      env = env,
       stdout = subprocess.PIPE,
       stderr = subprocess.STDOUT,
       text = True
