8afbc3733b3c1ba4094ab4184097084f9491e78e
theunbound <the.bentusi@gmail.com>
2024-09-24 03:02:03 +0200

    et køsystem, så PoolOutput ikke skrive oven i sig selv
    også lidt omnavngivning og omarrangering for at gøre PoolObject mere
    overskuelig

--- scripts/pool_output.py
+++ scripts/pool_output.py
@@ -41,6 +41,7 @@ yes, maybe, no, skip = ( text_effect( ch, st ) \
                         )
 
 class Output:
+   "implement PoolObject's interface for the old style output."
     def begin( self, pid, taskname ):
         self.taskname = "{:<42}".format(
             "\033[0;37;1m{}:\033[0m".format(taskname)
@@ -57,18 +58,20 @@ class Output:
         print( self.taskname + "\033[0;31m Failed!\033[0m" )
 
 class PoolOutput:
-   sized_caches_defaults = (( "acts", 0 )
-                           ,( "status", None )
+   sized_caches_defaults = (( "_acts", 0 )
+                           ,( "_status", None )
+                           ,( "_newtask", None )
                            )
    def __init__( self, n_processes, min_refresh_interval = 1 ):
-      self.clonk_count = 0
-      self.queue = tuple()
+      self._clonk_count = 0
+      self._queue = tuple()
+      self._commands = []
       self.n_processes = n_processes
-      self.resize_caches( 0 )
-      self.proc_map = dict()
-      self.max_act_queue = [0] * 4
-      self.bpm = min_refresh_interval
-      self.refresh = self.refresh_running
+      self._resize_caches( 0 )
+      self._proc_map = dict()
+      self._max_act_queue = [0] * 4
+      self._bpm = min_refresh_interval
+      self._follow_commands = self._command_follower
 
    @property
    def n_processes( self ):
@@ -85,10 +88,10 @@ class PoolOutput:
       except AttributeError:
          old_strs = []
       self.strs = [ s[ 1 - self.cell_width : ] for s in old_strs[:x] ]\
-                   + [ "." * min( self.clonk_count, self.cell_width - 1 ) ]\
+                   + [ "." * min( self._clonk_count, self.cell_width - 1 ) ]\
                      * ( x - len ( old_strs ) )
 
-   def resize_caches( self, new_size ):
+   def _resize_caches( self, new_size ):
       # sized caches with defaults:
       for s in self.sized_caches_defaults:
          try:
@@ -99,8 +102,20 @@ class PoolOutput:
                   old[:new_size] + [ s[1] ] * ( new_size - len( old ) )
                  )
 
-   def empty_caches( self ):
+   def _empty_caches( self ):
+      newtask_def = next( d for d in self.sized_caches_defaults
+                          if d[0] == "_newtask"
+                         )
+      try:
+         self._newtask = [ nt if st else newtask_def[1]
+                           for nt,st in zip( self._newtask, self._status )
+                          ]
+      except AttributeError:
+         pass
+      
       for s in self.sized_caches_defaults:
+         if s[0] == "_newtask":
+            continue
          try:
             l = len( getattr( self, s[0] ) )
          except AttributeError:
@@ -108,34 +123,34 @@ class PoolOutput:
          else:
             setattr( self, s[0], [ s[1] ] * l )
 
-   def map( self, pid ):
+   def _map( self, pid ):
       try:
-         tn = self.proc_map[ pid ]
+         tn = self._proc_map[ pid ]
       except KeyError:
-         tn = len( self.proc_map )
-         self.proc_map[ pid ] = tn
-         self.resize_caches( tn + 1 )
+         tn = len( self._proc_map )
+         self._proc_map[ pid ] = tn
+         self._resize_caches( tn + 1 )
 
       return tn
 
-   def map_drop( self, index ):
-      self.proc_map = { pid: (i if i < index else i - 1) \
-                        for pid, i in items( self.proc_map ) \
+   def _map_drop( self, index ):
+      self._proc_map = { pid: (i if i < index else i - 1) \
+                        for pid, i in items( self._proc_map ) \
                         if i != index
                        }
       for s in self.sized_caches_default:
          old = getattr( self, s[0] )
          setattr( self, s[0], old[ : index ] + old[ index+1 : ] )
 
-   def map_move( self, fr, to ):
+   def _map_move( self, fr, to ):
       for s in self.sized_caches_default:
          old = getattr( self, s[0] )
          old[ to ] = old[ fr ]
          setattr( self, s[0], old )
-      for pid in self.proc_map:
-         if self.proc_map[ pid ] == to:
-            self.map_drop( fr ) # probably breaks the iterator
-            self.proc_map[ pid ] = fr
+      for pid in self._proc_map:
+         if self._proc_map[ pid ] == to:
+            self._map_drop( fr ) # probably breaks the iterator
+            self._proc_map[ pid ] = fr
             break
       else:                     # nobreak
          # oh, dear ... maybe:
@@ -143,28 +158,17 @@ class PoolOutput:
             "proc_map has no entry pointing to index {}".format( to )
          )
 
-   def queue_add( self, *x ):
-      if not x:
-         return
-      self.pause()
+   def _queue_update_info( self, prns ):
       print( "\n  Tilføjet til kø:" )
-      prns = [ ( len( ind + ": " + fn ), task_start( ind + ": " ) + fn ) \
-               for ( fn, ind ) \
-               in  zip( x,
-                        islice( cycle(indices), len( self.queue ), None )
-                       )
-              ]
       print_columnized( *prns )
-      self.queue += x
-      self.resume()
 
-   def legend( self ):
+   def _legend( self ):
       print( "  Multiprocesmonitor:" )
       print( "Nuværende opgave: {}".format( task_start( indices[0] ) ), end="" )
       print( ", aktivitet: " + indicators, end="" )
       print( ", resultat: {}{}{}{}".format( yes, maybe, no, skip ) )
 
-   def proc_headers( self ):
+   def _proc_headers( self ):
       try:
          header_format = next(
             header for header in headers
@@ -187,118 +191,146 @@ class PoolOutput:
              ]
          ))
 
-   def preamble_once( self ):
+   def _preamble_once( self ):
+      self._preamble_once = lambda *_: None # never to be seen again
       print()
-      self.legend()
-      self.proc_headers()
-      self.clonk_time = time()
-      self.activity = self.activity_running
-      self.set_status = self.set_status_running
+      self._legend()
+      self._proc_headers()
+      self._clonk_time = time()
 
-   def activity( self, pid, number ):
-      self.preamble_once()
-      return self.activity( pid, taskname, number )
+   def _enqueue_command( self, *args, command = None ):
+      command = command or self._set_status
+      self._preamble_once()
+      self._commands += [( command, *args )]
+      self._follow_commands()
 
-   def set_status( self, pid, status ):
-      self.preamble_once()
-      return self.set_status( pid, status )
+   def _set_status( self, pid, status ):
+      tn = self._map( pid )
+      while self._status[ tn ] or self._newtask[ tn ]:
+         self._clonk()
+      self._status[ tn ] = status
 
-   def activity_running( self, pid, number ):
-      tn = self.map( pid )
-      self.acts[ tn ] += number
-
-   def set_status_running( self, pid, status ):
-      tn = self.map( pid )
-      if self.status[ tn ]:
-         self.clonk()
-
-      self.status[ tn ] = status
-
-   def begin( self, pid, taskname ):
+   def _set_newtask( self, pid, taskname ):
+      tn = self._map( pid )
+      while self._newtask[ tn ]:
+         self._clonk()
       try:
-         self.set_status( pid, task_start(
-            next( c for c,n in zip( cycle( indices ), self.queue ) \
+         self._newtask[ tn ] = task_start(
+            next( c for c,n in zip( cycle( indices ), self._queue ) \
                   if n == taskname
                  )
-         ))
+         )
       except StopIteration:
          raise KeyError( "Unqueued taskname: '{}'".format( taskname ) )
 
-   def success( self, pid ):
-      self.set_status( pid, yes )
-
-   def done_with_warnings( self, pid ):
-      self.set_status( pid, maybe )
-
-   def failed( self, pid ):
-      self.set_status( pid, no )
-
-   def skipped( self, pid ):
-      self.set_status( pid, skip )
-
-   def clonk( self ):
+   def _clonk( self ):
       # next line?
-      if self.clonk_count >= max( 1, self.cell_width - 1 ):
+      if self._clonk_count >= max( 1, self.cell_width - 1 ):
          print()
          self.strs = [ "" ] * len( self.strs )
-         self.clonk_count = 0
+         self._clonk_count = 0
 
       # decide which character to print
-      self.max_act_queue = self.max_act_queue[1:] + [ max( self.acts or [0] ) ]
-      max_act = max( self.max_act_queue ) or 1 # classic
+      self._max_act_queue = self._max_act_queue[1:] \
+         + [ max( self._acts or [0] ) ]
+      max_act = max( self._max_act_queue ) or 1 # classic
       for i,s in enumerate( self.strs ):
          try:
-            t = self.status[i]
+            t = self._status[i] or self._newtask[i]
          except IndexError:
             t = None
          if t:
             self.strs[i] += t
             continue
          try:
-            a = self.acts[i]
+            a = self._acts[i]
          except IndexError:
             a = 0
          self.strs[i] += indicators[
             ceil( a / max_act * (len( indicators ) - 1 ))
          ]
-      self.clonk_count += 1
-      self.clonk_time = time()
-      self.empty_caches()
+      self._clonk_count += 1
+      self._clonk_time = time()
+      self._empty_caches()
       
       # try to keep up with pid change
-      while len( self.proc_map ) > self.n_processes:
+      while len( self._proc_map ) > self.n_processes:
          try:
             inactive = next(
-               i for i, a, t in zip( count(), self.acts, self.status )
+               i for i, a, t in zip( count(), self._acts, self._status )
                if not a and not t
             )
          except StopIteration:
             break
          else:
-            self.map_move( self.n_processes, inactive )
+            self._map_move( self.n_processes, inactive )
 
-      # pull the lever, clonk
+      # pull the lever, _clonk
       print( "\r", end="" )
       for s in self.strs[ : self.n_processes ]:
-         print( s + " " * ( self.cell_width - self.clonk_count ), end="" )
+         print( s + " " * ( self.cell_width - self._clonk_count ), end="" )
          
-   def pause( self ):
-      self.refresh = lambda *_: None
-   def resume( self ):
-      self.refresh = self.refresh_running
-      self.refresh()
-
-   def refresh_running( self ):
+   def _refresh_running( self ):
       try:
-         if time() - self.clonk_time >= self.bpm:
-            self.clonk()
+         if time() - self._clonk_time >= self._bpm:
+            self._clonk()
       except AttributeError:
          pass
 
+   def _command_follower( self ):
+      # something, something, "reentrancy"...
+      self._follow_commands = lambda *_: None
+      while self._commands:
+         c, self._commands = self._commands[0], self._commands[1:]
+         c[0]( *c[1:] )
+      self._follow_commands = self._command_follower
+
    def end_output( self ):
-      self.clonk()
-      print()
+      "should only be called when no further signals are coming."
       
+      while any( self._status + self._newtask + self._acts ):
+         self._clonk()
+      print()
+
+   def queue_add( self, *x ):
+      "add arguments (strings) to the list of task names that "\
+         "might generate statuses."
+      if not x:
+         return
+      prns = [ ( len( ind + ": " + fn ), task_start( ind + ": " ) + fn ) \
+               for ( fn, ind ) \
+               in  zip( x,
+                        islice( cycle(indices), len( self._queue ), None )
+                       )
+              ]
+      self._commands += [( self._queue_update_info, prns )]
+      self._queue += x
+      self._follow_commands()
+
+   def activity( self, pid, number ):
+      "'number' might be counting lines of output from an external program."
+      self._preamble_once()
+      tn = self._map( pid )
+      self._acts[ tn ] += number
+
+   def begin( self, pid, taskname ):
+      """associates taskname and pid.
+      
+      there is no direct support for one pid switching between several tasks,
+      other than calling begin again for every switch."""
+      self._enqueue_command( pid, taskname, command = self._set_newtask )
+      
+   def success( self, pid ):
+      self._enqueue_command( pid, yes )
+   def done_with_warnings( self, pid ):
+      self._enqueue_command( pid, maybe )
+   def failed( self, pid ):
+      self._enqueue_command( pid, no )
+   def skipped( self, pid ):
+      self._enqueue_command( pid, skip )
+   def refresh( self ):
+      self._enqueue_command( command = self._refresh_running )
+
 
 def good_col_width( *widths ):
    "found by some arbitrary method. Takes widths as numbers."
